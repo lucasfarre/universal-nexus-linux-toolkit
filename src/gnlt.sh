46a4c09e7d5bf6216acc613c3c0c51434a145ec5
Lucas Farre <lucasfarre@gmail.com>
2012-08-01 21:10:42 -0300

    Version 1.0. Compatible with all Galaxy Nexus variants (maguro, toro, toroplus)

--- src/gnlt.sh
+++ src/gnlt.sh
@@ -1,29 +1,78 @@
 #!/bin/bash
-# Verizon Galaxy Nexus Linux Toolkit v0.2
+# Galaxy Nexus Linux Toolkit v1.0
 # by lucasfarre (tatelucas)
 # Apache License 2.0
 # Source: http://code.google.com/p/galaxy-nexus-linux-toolkit/
 # XDA Official Thread: Coming Soon...
 
-# Macros
+# Environment Variables
 
-FACTORY_IMAGE_URL="https://dl.google.com/dl/android/aosp/mysid-imm76k-factory-98d21321.tgz"
+PATH="$PATH:$PWD/platform-tools"
+
+# Constants
+
+TRUE=1
+FALSE=0
+INVALID=0
+MAGURO_DIR="$PWD/devices/maguro"
+TORO_DIR="$PWD/devices/toro"
+TOROPLUS_DIR="$PWD/devices/toroplus"
+FACTORY_MAGURO="https://dl.google.com/dl/android/aosp/yakju-jro03c-factory-3174c1e5.tgz"
+MD5_MAGURO="61cad184127144888445987249f06fca"
+FACTORY_TORO="https://dl.google.com/dl/android/aosp/mysid-imm76k-factory-98d21321.tgz"
+MD5_TORO="e76c6fe7d2bc799e5a6168f9dcb8c4ca"
+FACTORY_TOROPLUS=""
+MD5_TOROPLUS=""
 
 # Functions
 
 printHeader () {
 reset
 echo "********************************************************************************"
-echo "*              Verizon Galaxy Nexus Linux Toolkit 0.2 by tatelucas             *"
+echo "*                 Galaxy Nexus Linux Toolkit v1.0 by tatelucas                 *"
 echo "********************************************************************************"
+}
+
+printDeviceChooser () {
+  echo "G. GSM Galaxy Nexus [maguro] "
+  echo "V. Verizon Galaxy Nexus [toro]"
+  echo "S. Sprint Galaxy Nexus [toroplus]"
+  echo ""
+  echo -n "Please choose your device: "
+}
+
+setVariables () {
+  ERROR=$FALSE
+  case $DEVICE in
+    G) DEVICE_NAME="GSM Galaxy Nexus"
+       DEVICE_DIR=$MAGURO_DIR
+       FACTORY_URL=$FACTORY_MAGURO
+       MD5=$MD5_MAGURO;;
+    V) DEVICE_NAME="Verizon Galaxy Nexus"
+       DEVICE_DIR=$TORO_DIR
+       FACTORY_URL=$FACTORY_TORO;;
+    S) DEVICE_NAME="Sprint Galaxy Nexus"
+       DEVICE_DIR=$TOROPLUS_DIR
+       FACTORY_URL=$FACTORY_TOROPLUS;;
+    *) DEVICE=$INVALID;;
+  esac
+}
+
+printDevice () {
+  echo "Selected Device: $DEVICE_NAME"
+  echo ""
 }
 
 printMenu () {
   echo "1. Unlock the Bootloader"
   echo "2. Flash ClockworkMod Recovery"
   echo "3. Root the Device"
-  echo "4. Flash Google Factory Image"
-  echo "5. Exit"
+  echo "4. Lock the Bootloader"
+  if [ $DEVICE != "S" ];
+  then
+    echo "5. Flash Google Factory Image"
+  fi
+  echo "0. Exit"
   echo ""
   echo -n "Please choose a valid option: "
 }
@@ -33,7 +82,6 @@ returnMenu () {
   echo "Press ENTER to return to the main menu..."
   echo ""
   read KEY
-  FLAG=1
 }
 
 bootloader () {
@@ -63,9 +111,7 @@ printBootloaderManual () {
 }
 
 runBootloaderCommands () {
-  cd ./platform-tools
-  ./fastboot oem unlock
-  cd ..
+  fastboot oem unlock
 }
 
 recovery () {
@@ -81,9 +127,7 @@ printRecoveryHeader () {
 }
 
 runRecoveryCommands () {
-  cd ./platform-tools
-  ./fastboot flash recovery ./recovery/recovery.img
-  cd ..  
+  fastboot flash recovery $DEVICE_DIR/recovery/recovery.img
 }
 
 root () {
@@ -114,11 +158,9 @@ printRootManual () {
 }
 
 runRootCommands () {
-  cd ./platform-tools
-  ./adb shell mkdir /sdcard/GNLT
-  ./adb push ./root/su.zip /sdcard/GNLT
-  ./adb reboot recovery
-  cd ..
+  adb shell mkdir /sdcard/GNLT
+  adb push $DEVICE_DIR/root/su.zip /sdcard/GNLT
+  adb reboot recovery
 }
 
 factory () {
@@ -126,7 +168,12 @@ factory () {
   printFactoryHeader
   printBootloaderManual
   downloadFactoryImage
-  runFactoryCommands
+  checkFactoryImage
+  if [ $ERROR -eq $FALSE ];
+  then
+    extractFactoryImage
+    runFactoryCommands
+  fi
   returnMenu
  }
 
@@ -136,51 +183,76 @@ printFactoryHeader () {
 }
 
 downloadFactoryImage () {
-  cd ./platform-tools/factory
+  cd $DEVICE_DIR/factory
   if [ ! -e ./factory.tgz ];
   then
-  wget -O factory.tgz $FACTORY_IMAGE_URL
+    wget -O factory.tgz $FACTORY_URL
+    chmod 666 ./factory.tgz
+  fi
+}
+
+checkFactoryImage () {
+  if [ $MD5 != "`md5sum ./factory.tgz | head -c 32`" ];
+  then
+    ERROR=$TRUE
+    echo "ERROR: Google Factory Image is Corrupted. Erasing..."
+    rm ./factory.tgz
+    echo "Erased. Please try again later."
+  fi
+}
+
+extractFactoryImage () {
+  cd $DEVICE_DIR/factory
+  if [ `ls -d extract_here` != "" ];
+  then
+  rm -r ./extract_here
   fi
   tar -xzf ./factory.tgz
-  mv ./`ls -d */` ./extract
-  cd ./extract
-  mv `ls bootloader*` ./bootloader.img
-  mv `ls radio*` ./radio.img
-  mv `ls image*` ./image.zip
-  cd ..
-  cd ..
-  cd ..
+  mv ./`ls -d */` ./extract_here
+  chmod -R 666 ./extract_here
 }
 
 runFactoryCommands () {
-  cd ./platform-tools
-  ./fastboot flash bootloader ./factory/extract/bootloader.img
-  ./fastboot reboot-bootloader
-  sleep 5
-  ./fastboot flash radio ./factory/extract/radio.img
-  ./fastboot reboot-bootloader
-  sleep 5
-  ./fastboot -w update ./factory/extract/image.zip
-  cd ..
+  cd $DEVICE_DIR/factory/extract_here
+  bash ./flash-all.sh
+}
+
+lockBootloader () {
+  printLockHeader
+  printBootloaderManual
+  runLockCommands
+  returnMenu
+}
+
+printLockHeader () {
+  echo "Lock the Bootloader"
+  echo ""
+}
+
+runLockCommands () {
+  fastboot oem lock
 }
 
 exitToolkit () {
-  FLAG=0
+  EXIT=$TRUE
   reset
-}
-
-invalid () {
-  reset
-  echo "Invalid Option."
-  returnMenu
 }
 
 # Main
 
-FLAG=1
-while [ $FLAG -eq 1 ]
+DEVICE=$INVALID
+while [ $DEVICE -eq $INVALID ]
 do
   printHeader
+  printDeviceChooser
+  read DEVICE
+  setVariables
+done
+EXIT=$FALSE
+while [ $EXIT -eq $FALSE ]
+do
+  printHeader
+  printDevice
   printMenu
   read OPTION
   printHeader
@@ -188,9 +260,15 @@ do
     1) bootloader;;
     2) recovery;;
     3) root;;
-    4) factory;;
-    5) exitToolkit;;
-    *) invalid;;
+    4) lockBootloader;;
+    5) if [ $DEVICE == "S" ];
+       then
+         returnMenu
+       else
+         factory
+       fi;;
+    0) exitToolkit;;
+    *) ;;
   esac
 done
 exit
